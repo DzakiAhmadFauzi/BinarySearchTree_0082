6435d70ad4b226923db79a196799787b652f723f
DzakiAhmadFauzi <dzakfauzi229@gmail.com>
2025-06-26 18:40:04 +0700

    step 5

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -50,4 +50,14 @@ public;
         Node *currentNode = nullptr;
         search(x, parent, currentNode);
 
+        // Step 5: If parent is NULL (Tree is empty)
+        if (parent == nullptr)
+        {
+            // 5a: Mark the new node as ROOT
+            ROOT = newNode;
+
+            // 5b: Exit
+            return;
+        }
+
         
