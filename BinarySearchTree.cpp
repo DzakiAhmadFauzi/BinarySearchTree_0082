0948f266adda6402fae970cbcb9dd5f7b3cfe299
DzakiAhmadFauzi <dzakfauzi229@gmail.com>
2025-06-26 18:16:36 +0700

    commit 3

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -4,3 +4,18 @@
 #include <iostream>
 using namespace std;
 
+class Node
+{
+public:
+    int info;
+    Node *leftchild;
+    Node *rightchild;
+
+    // Constructor for the node class
+    Node()
+    {
+        leftchild = nullptr; // Initialize left child to null
+        rightchild = nullptr; // Initialize right child to null
+    }
+};
+
